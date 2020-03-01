430a2c987d67db5085c23dd56ef0db4157ecab79
ys <yunshen.yang@foxmail.com>
2020-03-01 19:00:00 +0800

    add new view type

--- echart_views/models/models.py
+++ echart_views/models/models.py
@@ -9,9 +9,3 @@ class Game(models.Model):
     name = fields.Char('游戏名', required=True)
     downloads = fields.Integer(string='下载量', default=0)
     platform = fields.Char(string='平台')
-
-    @api.model
-    def get_game_list(self):
-        all_games = self.search([])
-        game_list = [{'value': g.downloads, 'name': g.name} for g in all_games]
-        return dict(game_list=game_list)
